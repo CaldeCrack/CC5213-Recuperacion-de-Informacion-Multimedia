823e6694ffcae9c98e65e0f1169efa4246d564a4
Andrés Calderón <an.caldeguardia@gmail.com>
2023-08-20 21:46:10 -0400

    index

--- Tareas/Tarea_1/tarea1-buscar.py
+++ Tareas/Tarea_1/tarea1-buscar.py
@@ -2,8 +2,7 @@
 # 15 de agosto de 2023
 # Alumno: Andrés Calderón Guardia
 
-import sys
-import os.path
+import sys, os.path, numpy, cv2, scipy
 
 def tarea1_buscar(dir_input_imagenes_Q, dir_input_descriptores_R, file_output_resultados):
     if not os.path.isdir(dir_input_imagenes_Q):
@@ -16,19 +15,51 @@ def tarea1_buscar(dir_input_imagenes_Q, dir_input_descriptores_R, file_output_re
         print("ERROR: ya existe archivo {}".format(file_output_resultados))
         sys.exit(1)
     # Implementar la tarea:
-    #  1-leer imágenes en dir_input_imagenes_Q y calcular descriptores cada imagen
-    #    for nombre in os.listdir(dir_input_imagenes_Q):
-    #        if not nombre.endswith(".jpg"):
-    #            continue
-    #        archivo_imagen = "{}/{}".format(dir_input_imagenes_Q, nombre)
-    #  2-leer descriptores de R de dir_input_descriptores_R
-    #  3-para cada descriptor q localizar el mas cercano en R
-    #  4-escribir en file_output_resultados haciendo print() con el formato: 
-    #    with open(file_output_resultados, 'w') as f:
-    #        for ....
-    #            print("{}\t{}\t{}".format(imagen_q, imagen_r, distancia), file=f)
-    # borrar la siguiente linea
-    print("ERROR: no implementado!")
+    #* 1-leer imágenes en dir_input_imagenes_Q y calcular descriptores cada imagen
+    for nombre in os.listdir(dir_input_imagenes_Q):
+        if not nombre.endswith(".jpg"):
+            continue
+        archivo_imagen = "{}/{}".format(dir_input_imagenes_Q, nombre)
+        # divisiones
+        num_zonas_x = 2
+        num_zonas_y = 2 
+        num_bins_por_zona = 8
+        ecualizar = True
+        # leer imagen
+        imagen = cv2.imread(archivo_imagen, cv2.IMREAD_GRAYSCALE)
+        if ecualizar:
+            imagen = cv2.equalizeHist(imagen)
+        # procesar cada zona
+        descriptor = []
+        for j in range(num_zonas_y):
+            desde_y = int(imagen.shape[0] / num_zonas_y * j)
+            hasta_y = int(imagen.shape[0] / num_zonas_y * (j+1))
+            for i in range(num_zonas_x):
+                desde_x = int(imagen.shape[1] / num_zonas_x * i)
+                hasta_x = int(imagen.shape[1] / num_zonas_x * (i+1))
+                # recortar zona de la imagen
+                zona = imagen[desde_y : hasta_y, desde_x : hasta_x]
+                # histograma de los pixeles de la zona
+                histograma, limites = numpy.histogram(zona, bins=num_bins_por_zona, range=(0,255))
+                # normalizar histograma (bins suman 1)
+                histograma = histograma / numpy.sum(histograma)
+                # agregar descriptor de la zona al descriptor global
+                descriptor.extend(histograma)
+        # agregar descriptor a la matriz de descriptores
+        if len(matriz_descriptores) == 0:
+            matriz_descriptores = descriptor
+        else:
+            matriz_descriptores = numpy.vstack([matriz_descriptores, descriptor])
+    
+    #* 2-leer descriptores de R de dir_input_descriptores_R
+    descriptores_R = []
+    #* 3-para cada descriptor q localizar el mas cercano en R
+    matriz_distancias = scipy.spatial.distance.cdist(matriz_descriptores, matriz_descriptores, metric='cityblock')
+    #* 4-escribir en file_output_resultados haciendo print() con el formato: 
+    with open(file_output_resultados, 'w') as f:
+        pass
+        # for ...
+        #     print("{}\t{}\t{}".format(imagen_q, imagen_r, distancia), file=f)
 
 
 # inicio de la tarea
--- Tareas/Tarea_1/tarea1-indexar.py
+++ Tareas/Tarea_1/tarea1-indexar.py
@@ -2,8 +2,7 @@
 # 15 de agosto de 2023
 # Alumno: Andrés Calderón Guardia
 
-import sys
-import os.path
+import sys, os.path, numpy, cv2
 
 def tarea1_indexar(dir_input_imagenes_R, dir_output_descriptores_R):
     if not os.path.isdir(dir_input_imagenes_R):
@@ -12,15 +11,52 @@ def tarea1_indexar(dir_input_imagenes_R, dir_output_descriptores_R):
     elif os.path.exists(dir_output_descriptores_R):
         print("ERROR: ya existe directorio {}".format(dir_output_descriptores_R))
         sys.exit(1)
-    # Implementar la tarea:
-    #  1-leer imágenes en dir_input_imagenes_R y calcular descriptores cada imagen
+    #* 1-leer imágenes en dir_input_imagenes_R y calcular descriptores cada imagen
+    lista_nombres = []
+    matriz_descriptores = []
     for nombre in os.listdir(dir_input_imagenes_R):
         if not nombre.endswith(".jpg"):
             continue
         archivo_imagen = "{}/{}".format(dir_input_imagenes_R, nombre)
-    #  3-escribir en dir_output_descriptores_R los descriptores calculados (crear uno o más archivos)
+        # divisiones
+        num_zonas_x = 2
+        num_zonas_y = 2 
+        num_bins_por_zona = 8
+        ecualizar = True
+        # leer imagen
+        imagen = cv2.imread(archivo_imagen, cv2.IMREAD_GRAYSCALE)
+        if ecualizar:
+            imagen = cv2.equalizeHist(imagen)
+        # procesar cada zona
+        descriptor = []
+        for j in range(num_zonas_y):
+            desde_y = int(imagen.shape[0] / num_zonas_y * j)
+            hasta_y = int(imagen.shape[0] / num_zonas_y * (j+1))
+            for i in range(num_zonas_x):
+                desde_x = int(imagen.shape[1] / num_zonas_x * i)
+                hasta_x = int(imagen.shape[1] / num_zonas_x * (i+1))
+                # recortar zona de la imagen
+                zona = imagen[desde_y : hasta_y, desde_x : hasta_x]
+                # histograma de los pixeles de la zona
+                histograma, limites = numpy.histogram(zona, bins=num_bins_por_zona, range=(0,255))
+                # normalizar histograma (bins suman 1)
+                histograma = histograma / numpy.sum(histograma)
+                # agregar descriptor de la zona al descriptor global
+                descriptor.extend(histograma)
+        # agregar descriptor a la matriz de descriptores
+        if len(matriz_descriptores) == 0:
+            matriz_descriptores = descriptor
+        else:
+            matriz_descriptores = numpy.vstack([matriz_descriptores, descriptor])
+        # agregar nombre del archivo a la lista de nombres
+        lista_nombres.append(nombre)
+
+    #* 3-escribir en dir_output_descriptores_R los descriptores calculados (crear uno o más archivos)
     os.makedirs(dir_output_descriptores_R, exist_ok=True)
     archivo_salida = "{}/{}".format(dir_output_descriptores_R, "descriptores.data")
+    with open(archivo_salida, "w") as f:
+        for i in range(len(matriz_descriptores)):
+            f.write(str(lista_nombres[i]) + ":" + str(matriz_descriptores[i]) + "\n")
 
 
 # inicio de la tarea
